f1eb51950b93d1a4f5a2d4fe3aa055be8aabf5ef
Chibong <102298656+Chibong3020@users.noreply.github.com>
2022-04-24 19:21:01 +0530

    THE MIGHT CHIBONG WILL RISE
    this is goood

---
+++ Guess the number.py
@@ -0,0 +1,13 @@
+import random
+
+number = random.randint(1, 20)
+guess = int(input("Guess a number between 1 and 20: "))
+
+while guess != number:
+    if guess > number:
+        guess = int(input("Too big, try again: "))
+    elif guess < number:
+        guess = int(input("Too small, try again: "))
+
+if guess == number:
+    print (f"Your guess was correct. It was {number}")
